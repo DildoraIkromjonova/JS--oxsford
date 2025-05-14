4c06addd07799210c72d71c7940dc120787cb5fd
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-05-14 22:08:00 +0500

    bajarildi

---
+++ Homeworks/lesson 6/index.js
@@ -0,0 +1,8 @@
+let yosh = prompt("18");
+let talaba = prompt("ha");
+
+if (yosh < 18 || talaba === "ha") {
+  console.log("Chipta narxi: 5000 so'm");
+} else {
+  console.log("Chipta narxi: 10000 so'm");
+}
