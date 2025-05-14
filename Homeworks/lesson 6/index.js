e62100114ba18d38260504fb0b0e9e394c580d94
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-05-14 22:12:32 +0500

    bajarildi

--- Homeworks/lesson 6/index.js
+++ Homeworks/lesson 6/index.js
@@ -1,7 +1,7 @@
-let yosh = prompt("18");
-let talaba = prompt("ha");
+let yosh = prompt("sizning yoshingiz necida");
+let talaba = prompt("siz talabamisiz");
 
-if (yosh < 18 || talaba === "ha") {
+if (yosh < 18 || talaba == "ha") {
   console.log("Chipta narxi: 5000 so'm");
 } else {
   console.log("Chipta narxi: 10000 so'm");
