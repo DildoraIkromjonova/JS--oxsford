4d0eaa1435530d9e51c137a1bc04b00ebabcd3f7
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-05-14 21:12:07 +0500

    ishladi

--- Lessons/lesson 3/index.js
+++ Lessons/lesson 3/index.js
@@ -1,13 +1,13 @@
-// let id=Symbol(123)
-// let id2=Symbol(123)
-// console.log(id==id2)
-// console.log(id===id2)
+let id=Symbol(123)
+let id2=Symbol(123)
+console.log(id==id2)
+console.log(id===id2)
 
-// let user={
-//     name: "toshmat",
-//     5:10,
-// };
+let user={
+    name: "toshmat",
+    5:10,
+};
 
-// console.log(user[5]);
+console.log(user[5]);
 
 
