545217e7c1a13d8e6079cc31f1758bd8a58e794d
DildoraIkromjonova <ikromjonovadildora1@gmail.com>
2025-05-15 13:47:50 +0500

    ffdef

--- Homeworks/6 dars/index.js
+++ Homeworks/6 dars/index.js
@@ -1,10 +1,10 @@
 
-let yosh = prompt("Yoshingiz nechida?");
-let talaba = prompt("Siz talabamisiz? (ha yoki yo'q)");
+    let yosh = prompt("Yoshingiz nechida?");
+    let talaba = prompt("Siz talabamisiz? (ha yoki yo'q)");
 
-if (yosh < 18 || talaba === "ha") {
-  console.log("Chipta narxi: 5000 so'm");
-} else {
-  console.log("Chipta narxi: 10000 so'm");
-}
+    if (yosh < 18 || talaba === "ha") {
+    console.log("Chipta narxi: 5000 so'm");
+    } else {
+    console.log("Chipta narxi: 10000 so'm");
+    }
 
